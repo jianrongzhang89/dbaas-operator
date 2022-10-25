0f61910c9c2149725c3cabcbf27e4d5348395993
jianrongzhang89 <jianrongzhang89@gmail.com>
2022-10-25 15:28:49 -0400

    POC for vault integration

--- controllers/dbaasinventory_controller.go
+++ controllers/dbaasinventory_controller.go
@@ -40,7 +40,6 @@ type DBaaSInventoryReconciler struct {
 //+kubebuilder:rbac:groups=dbaas.redhat.com,resources=*/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=dbaas.redhat.com,resources=*/finalizers,verbs=update
 //+kubebuilder:rbac:groups="",resources=secrets,verbs=get;patch
-//+kubebuilder:rbac:groups="",resources=serviceAccounts,verbs=get;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
